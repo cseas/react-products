e69300f0d7ca5e34ab48532e3af39c3b02e2e3f3
abhijeetsinghtomar <abhijeetsinghemail@gmail.com>
2019-12-22 03:29:33 +0530

    Updates

---
+++ precache-manifest.be6b8af6a99d14c2cb5fe89d7d2ac6c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "485f92107f795af01782c4e35c0af698",
+    "url": "/react-products/index.html"
+  },
+  {
+    "revision": "72b48553a7fabe356acb",
+    "url": "/react-products/static/js/2.6dbfa46a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-products/static/js/2.6dbfa46a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "123f5162bbd7695128e6",
+    "url": "/react-products/static/js/main.172eddf3.chunk.js"
+  },
+  {
+    "revision": "ea617b0d2434bc39b4dc",
+    "url": "/react-products/static/js/runtime-main.9ae566db.js"
+  }
+]);
