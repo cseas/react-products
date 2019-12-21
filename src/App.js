350bac0d75a0cc91e7ec2cb68a19849ebc1a1e55
abhijeetsinghtomar <abhijeetsinghemail@gmail.com>
2019-12-22 03:32:49 +0530

    configure deployment

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Product from "./Product";
+import productsData from "./productsData"
+
+function App() {
+  const productsComponents = productsData.map(
+    item => <Product key={item.id} product={item} />);
+
+    return (
+      <div>
+        {productsComponents}
+      </div>
+    );
+}
+
+export default App;
